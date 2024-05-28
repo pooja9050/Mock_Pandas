b48bf974572a9a41d356bc983a985f99033288b7
Pooja Madhup <90482825+pooja9050@users.noreply.github.com>
2024-05-28 06:24:36 -0700

    Update Mock_Pandas.py

--- Mock_Pandas.py
+++ Mock_Pandas.py
@@ -31,3 +31,47 @@ def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
     y = cinema[(cinema['description']!='boring') & (cinema['id'] % 2 != 0)]
     #Result should be in descending order of rating with all the columns with the filtered data of odd id and not including boring description
     return y.sort_values(by = 'rating', ascending=False)
+
+
+#1084. Sales Analysis III
+import pandas as pd
+
+def sales_analysis(product: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
+    q1_2019 = sales[(sales['sale_date'] >= '2019-01-01') & (sales['sale_date'] <= '2019-03-31')]
+    other_q = sales[(sales['sale_date'] < '2019-01-01') | (sales['sale_date'] > '2019-03-31')]
+    exclusive_ids = q1_2019[~q1_2019['product_id'].isin(other_q['product_id'].unique())]['product_id'].unique()
+    return product[product['product_id'].isin(exclusive_ids)][['product_id', 'product_name']]
+
+import pandas as pd
+
+def sales_analysis(product: pd.DataFrame, sales: pd.DataFrame) -> pd.DataFrame:
+    df1 = sales[(sales['sale_date']>= '2019-01-01') & (sales['sale_date'] <= '2019-03-31')]
+    df2 = sales[~((sales['sale_date']>= '2019-01-01') & (sales['sale_date'] <= '2019-03-31'))]
+    df3 = df1.merge(df2, left_on = 'product_id', right_on = 'product_id', how = 'left')
+    df4 = df3[df3['price_y'].isna()]
+    df = df4.merge(product, left_on = 'product_id', right_on = 'product_id', how = 'inner')
+    return df[['product_id', 'product_name']].drop_duplicates()
+
+
+#1158. Market Analysis I
+import pandas as pd
+
+def market_analysis(users: pd.DataFrame, orders: pd.DataFrame, items: pd.DataFrame) -> pd.DataFrame:
+    orders['orders_in_2019'] = orders['order_date'].apply(lambda x: 1 if x.year==2019 else 0)
+    df = orders.groupby('buyer_id')['orders_in_2019'].agg('sum').reset_index()
+    df = users.merge(df, left_on='user_id', right_on='buyer_id', how='left')
+    df['orders_in_2019'] = df['orders_in_2019'].fillna(0).astype(int)
+    return df[['user_id', 'join_date', 'orders_in_2019']].rename(columns={'user_id': 'buyer_id'})
+
+import pandas as pd
+
+def market_analysis(users: pd.DataFrame, orders: pd.DataFrame, items: pd.DataFrame) -> pd.DataFrame:
+    orders['order_date'] = pd.to_datetime(orders['order_date']) # Convert order_date to datetime
+    orders['order_date'] = orders['order_date'].dt.year
+    orders = orders[orders['order_date'] == 2019]
+    df = orders.groupby('buyer_id').size().reset_index(name='orders_in_2019')
+    df = users.merge(df, left_on='user_id', right_on='buyer_id', how='left')
+    df['orders_in_2019'] = df['orders_in_2019'].fillna(0).astype(int)
+    df = df[['user_id', 'join_date', 'orders_in_2019']]
+    return df.rename(columns={'user_id': 'buyer_id'})
+
